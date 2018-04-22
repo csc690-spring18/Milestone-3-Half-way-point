43ba677a822342cbfc1b3b4dea973e8bea5e18f8
tkevans <tkevans4@gmail.com>
2018-04-22 03:25:20 -0700

    change UI

--- FoodHaus_App/project-swift/Controller/Menu/CheckViewController.swift
+++ FoodHaus_App/project-swift/Controller/Menu/CheckViewController.swift
@@ -14,7 +14,6 @@ class CheckViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
     @IBAction func profileButton(_ sender: Any) {
         // check whether user has valid Auth session Firebase
         Auth.auth().addStateDidChangeListener() { auth, user in
@@ -27,36 +26,37 @@ class CheckViewController: UIViewController {
             }
         }
     }
-    
 
-    @IBAction func confirmButton(_ sender: Any) {
+    
+    
+    @IBAction func confirmBtn(_ sender: Any) {
         // check whether user has valid Auth session Firebase
         Auth.auth().addStateDidChangeListener() { auth, user in
             if user != nil {
-
+                
                 // check whether use fill in their info of profile
                 let ref = Database.database().reference()
                 let userID = Auth.auth().currentUser?.uid
-
+                
                 ref.child("users").child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
-
+                    
                     let value = snapshot.value as? NSDictionary
                     // user could check out if profile has enough info
                     if (value?["name"] as? String ?? "" != ""  &&
                         value?["address"] as? String ?? "" != "" &&
                         value?["phone"] as? String ?? "" != "") {
-
+                        
                         let vc = self.storyboard?.instantiateViewController(withIdentifier: "Confirm")
                         self.present(vc!, animated: true, completion: nil)
-
+                        
                     } else {
                         let alertController = UIAlertController(title: "Oops!", message: "Please complete Profile, we could contact and delivery to you!", preferredStyle: .alert)
-
+                        
                         let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
                         alertController.addAction(defaultAction)
-
+                        
                         self.present(alertController, animated: true, completion: nil)
-
+                        
                         //                        print("Testing")
                         //
                         //                        self.dismiss(animated: false) {
@@ -74,6 +74,7 @@ class CheckViewController: UIViewController {
             }
         }
     }
+    
   
     
 }
